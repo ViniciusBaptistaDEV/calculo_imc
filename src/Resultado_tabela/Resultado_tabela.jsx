be9eb67387dd37fd226da51b23aa227e30f0607d
Vinicius Baptista <viniciusb_s@hotmail.com>
2025-03-23 17:10:40 -0300

    Implementação da mensagem de retorno

--- src/Resultado_tabela/Resultado_tabela.jsx
+++ src/Resultado_tabela/Resultado_tabela.jsx
@@ -2,50 +2,47 @@ import { useState } from 'react';
 import styles from './resultado_tabela.module.css';
 import { useEffect } from 'react';
 
-export default function resultado_tabela({ resultadoIMC }) {
+export default function Resultado_tabela({ resultadoIMC }) {
 
+    const [mensagemIMC, setMensagemIMC] = useState('');
 
-    function mensagemResultado({ resultadoIMC }) {
+    useEffect(() => {
 
-        const [mensagemIMC, setMensagemIMC] = useState('');
+        const IMCNumerico = parseFloat(resultadoIMC.replace(',', '.'));
 
-        useEffect(() => {
+        if (IMCNumerico < 18.5) {
 
-            if (resultadoIMC < 18.5) {
+            setMensagemIMC('Seu IMC indica que voce está abaixo do peso ideal.' +
+                'Considere buscar uma alimentação balanceada e consultar um nutricionista para um plano alimentar adequado.')
 
-                setMensagemIMC('Seu IMC indica que voce está abaixo do peso ideal.' +
-                    'Considere buscar uma alimentação balanceada e consultar um nutricionista para um plano alimentar adequado.')
-    
-            } else if (resultadoIMC >= 18.5 && resultadoIMC < 24.9) {
-    
-                setMensagemIMC('Parabéns! Seu IMC está dentro da faixa considerada saudável.' +
-                    'Mantenha hábitos saudáveis com uma boa alimentação e exercícios regulares.')
-    
-            } else if (resultadoIMC >= 25.0 && resultadoIMC < 29.9) {
-    
-                setMensagemIMC('Seu IMC indica sobrepeso.' +
-                    'Talvez seja interessante adotar hábitos mais saudáveis, como uma dieta equilibrada e exercícios físicos regulares.')
-    
-            } else if (resultadoIMC >= 30.0 && resultadoIMC < 34.9) {
-    
-                setMensagemIMC('Seu IMC indica obesidade grau 1.' +
-                    'Reavaliar seus hábitos alimentares e incluir mais atividade física pode ser essencial para sua saúde.')
-    
-            } else if (resultadoIMC >= 35.0 && resultadoIMC < 39.9) {
-    
-                setMensagemIMC('Seu IMC indica obesidade grau 2 (severa).' +
-                    'É altamente recomendável procurar um profissional de saúde para orientações específicas.')
-    
-            } else if (resultadoIMC >= 40.0) {
-    
-                setMensagemIMC('Seu IMC indica obesidade grau 3 (mórbida).' +
-                    'Essa condição pode trazer sérios riscos à saúde. Procure acompanhamento médico e nutricional o quanto antes.')
-    
-            }
+        } else if (IMCNumerico >= 18.5 && IMCNumerico < 24.9) {
 
-        }, [resultadoIMC]);
+            setMensagemIMC('Parabéns! Seu IMC está dentro da faixa considerada saudável.' +
+                'Mantenha hábitos saudáveis com uma boa alimentação e exercícios regulares.')
 
-    }
+        } else if (IMCNumerico >= 25.0 && IMCNumerico < 29.9) {
+
+            setMensagemIMC('Seu IMC indica sobrepeso.' +
+                'Talvez seja interessante adotar hábitos mais saudáveis, como uma dieta equilibrada e exercícios físicos regulares.')
+
+        } else if (IMCNumerico >= 30.0 && IMCNumerico < 34.9) {
+
+            setMensagemIMC('Seu IMC indica obesidade grau 1.' +
+                'Reavaliar seus hábitos alimentares e incluir mais atividade física pode ser essencial para sua saúde.')
+
+        } else if (IMCNumerico >= 35.0 && IMCNumerico < 39.9) {
+
+            setMensagemIMC('Seu IMC indica obesidade grau 2 (severa).' +
+                'É altamente recomendável procurar um profissional de saúde para orientações específicas.')
+
+        } else if (IMCNumerico >= 40.0) {
+
+            setMensagemIMC('Seu IMC indica obesidade grau 3 (mórbida).' +
+                'Essa condição pode trazer sérios riscos à saúde. Procure acompanhamento médico e nutricional o quanto antes.')
+
+        }
+
+    }, [resultadoIMC]);
 
 
     return (
@@ -54,7 +51,7 @@ export default function resultado_tabela({ resultadoIMC }) {
 
             <div className='pt-3'>
                 <span className='fs-5 text-center'>Resultado do seu IMC: {resultadoIMC} </span>
-                <span> {mensagemIMC} </span>
+                <p> {mensagemIMC} </p>
             </div>
 
             <h5 className='my-4 fs-4 border-top border-secondary  pt-3'>Tabela de referência (IMC)</h5>
